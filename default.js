6ea20f62bc83bf1f1fb9ead03075b3c59eb99794
nayan-28 <nayanmalakar720@gmail.com>
2024-01-30 21:52:58 +0600

    learned default

---
+++ default.js
@@ -0,0 +1,20 @@
+function add(num1, num2 = 0) {
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    return result;
+}
+
+const sum = add(6, 7);
+
+function fullName(first, last = '') {
+    const full = first + ' ' + last;
+    return full;
+}
+
+function friend(nums[]) {
+
+}
+
+function person(human = {}) {
+
+}
