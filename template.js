23972f54bc8fce25f268a2ac57d282af4435ae0e
nayan-28 <nayanmalakar720@gmail.com>
2024-01-30 22:11:50 +0600

    conacate

---
+++ template.js
@@ -0,0 +1,13 @@
+const first = 'Nayan';
+const last = 'Malakar';
+
+
+const name = first + ' ' + last;
+console.log(name);
+
+
+const a = 10;
+const b = 15;
+
+const math = `The sum of ${a} an ${b} is ${a+b}`;
+console.log(math);
